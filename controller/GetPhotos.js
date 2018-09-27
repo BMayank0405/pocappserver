93e118d8154d43f022a38b9e09d23cd9dfa4b1cb
BMayank0405 <mayankbarthwal58@gmail.com>
2018-09-27 06:58:11 +0530

    data

--- controller/GetPhotos.js
+++ controller/GetPhotos.js
@@ -1,15 +1,25 @@
-
 const Photo = require("../model/photo");
 
 module.exports = {
-  getPhotos: async (req, res, next) => {
-    let { startTime, endTime } = req.body;
+  getPhotos: async(req, res, next) => {
+    let {
+      startTime,
+      endTime
+    } = req.body;
+    console.log(startTime, endTime)
     try {
-      let photos_Array = await Photo.find({ timestamp: { $gte: startTime, $lt: endTime } })
+      let photos_Array = await Photo.find({
+        timestamp: {
+          $gte: startTime,
+          $lt: endTime
+        }
+      })
 
       res.status(200).json(photos_Array);
     } catch (err) {
-      if (err) return res.status(500).send({ error: err.errmsg });
+      if (err) return res.status(500).send({
+        error: err.errmsg
+      });
     }
   }
 };
