8087b2bb5e44ed290c2b71793c2867b0a582d455
BMayank0405 <mayankbarthwal58@gmail.com>
2018-09-30 03:01:29 +0530

    fetch correct images

--- controller/GetPhotos.js
+++ controller/GetPhotos.js
@@ -4,19 +4,34 @@ module.exports = {
   getPhotos: async(req, res, next) => {
     let {
       startTime,
-      endTime
+      endTime,
+      email
     } = req.body;
 
     try {
+      let user = await User.findOne({
+        email
+      });
 
-      let photos_Array = await Photo.find({
-        timestamp: {
-          $gte: startTime,
-          $lt: endTime
-        }
-      })
+      try {
+        let photos_Array = await Photo.find({
+          $and: [{
+            timestamp: {
+              $gte: startTime,
+              $lt: endTime
+            }
+          }, {
+            userId: user._id
+          }]
+        })
 
-      res.status(200).json(photos_Array);
+        res.status(200).json(photos_Array);
+      } catch (e) {
+        console.log(err)
+        if (err) return res.status(500).send({
+          "msg": "Image fetch has failed.Please try again later."
+        });
+      }
     } catch (err) {
       {
         console.log(err)
